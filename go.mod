9d6c99944e14b07ffe92f24bc09169cae207394e
AJ Hammond <aj.hammond@praetorian.com>
2026-04-19 23:00:43 -0400

    chore(pki): add package skeleton and pin go-pkcs12 dependency

--- go.mod
+++ go.mod
@@ -10,4 +10,5 @@ require (
 require (
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/spf13/pflag v1.0.9 // indirect
+	software.sslmate.com/src/go-pkcs12 v0.4.0 // indirect
 )
---
+++ internal/pki/doc.go
@@ -0,0 +1,9 @@
+// Package pki provides X.509 + PFX + CSR primitives used across certigo subcommands.
+//
+// The pki package is deliberately narrow: parse/build certificates, read/write
+// PFX blobs, generate RSA keys, and construct CSRs with arbitrary extensions
+// (including UPN-in-SAN for AD CS enrollment). Higher-level features like
+// certificate forging or Schannel TLS are in other packages.
+package pki
+
+import _ "software.sslmate.com/src/go-pkcs12"
